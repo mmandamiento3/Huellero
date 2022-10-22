fdd6d7d44108755843d1a969529441c978b9977b
Christian Morales <mmandamiento3@gmail.com>
2022-10-22 00:43:21 -0500

    funcionalidades boton Iniciar

--- Huellero/Huellero/Controllers/TblUsuariosController.cs
+++ Huellero/Huellero/Controllers/TblUsuariosController.cs
@@ -10,6 +10,7 @@ using SecuGen.FDxSDKPro.Windows;
 using NuGet.Packaging;
 using NuGet.Protocol.Plugins;
 using Microsoft.AspNetCore.Components.Web;
+using System.Drawing;
 
 namespace Huellero.Controllers
 {
@@ -18,8 +19,8 @@ namespace Huellero.Controllers
         private readonly PruebaContext _context;
         private SGFingerPrintManager m_FPM;
         //private bool m_LedOn = false;
-        //private Int32 m_ImageWidth;
-        //private Int32 m_ImageHeight;
+        private Int32 m_ImageWidth;
+        private Int32 m_ImageHeight;
         //private Byte[] m_RegMin1 = null;
         //private Byte[] m_RegMin2;
         //private Byte[] m_VrfMin;
@@ -31,7 +32,8 @@ namespace Huellero.Controllers
 
             m_FPM = new SGFingerPrintManager();
             Listar_Dispositivos();
-            AbrirDispositivo(0);
+            GenerarHuella();
+            
         }
          private List<SelectListItem> Listar_Dispositivos()
         {
@@ -241,32 +243,21 @@ namespace Huellero.Controllers
         //    //return  Json(AbrirDispositivo(idSeleccionado));
         //}
 
-        public int  AbrirDispositivo(int ValorSeleccionado)
+        [HttpPost]
+        public async Task<JsonResult> AbrirDispositivo(int ValorSeleccionado)
         {
-            //if (m_FPM.NumberOfDevice == 0)
-            //    return 1;
+            if (m_FPM.NumberOfDevice == 0)
+                return Json(6);
 
             SGFPMDeviceName device_name;
             int device_id;
-            int iError;
-
-            //Int32 numberOfDevices = comboBoxDeviceName.Items.Count;
-            //Int32 deviceSelected = comboBoxDeviceName.SelectedIndex;
-            //Boolean autoSelection = (deviceSelected == (numberOfDevices - 1));  // Last index
-
-            //if (autoSelection)
-            //{
-            //    // Order of search: Hamster IV(HFDU04) -> Plus(HFDU03) -> III (HFDU02)
-            //    device_name = SGFPMDeviceName.DEV_AUTO;
-
-            //    device_id = (Int32)(SGFPMPortAddr.USB_AUTO_DETECT);
-            //}
+            int iError;      
          
-                device_name = m_DevList[ValorSeleccionado].DevName;
-                device_id = m_DevList[ValorSeleccionado].DevID;
+            device_name = m_DevList[ValorSeleccionado].DevName;
+            device_id = m_DevList[ValorSeleccionado].DevID;
 
-                iError = OpenDevice2(device_name, device_id);
-            return iError;
+            iError = OpenDevice2(device_name, device_id);
+            return Json(iError);
         }
 
 
@@ -274,136 +265,62 @@ namespace Huellero.Controllers
         {
             int iError = m_FPM.Init(device_name);
             iError = m_FPM.OpenDevice(device_id);
+            
 
-           // CheckBoxAutoOn.Enabled = false;
+           return iError;
+        }
+
+        private void Obtener()
+        {
+            SGFPMDeviceInfoParam pInfo = new SGFPMDeviceInfoParam();
+            int iError = m_FPM.GetDeviceInfo(pInfo);
+
             if (iError == (int)SGFPMError.ERROR_NONE)
             {
-                //GetBtn_Click(sender, e);
-                //GetBtn_Click(null, null);
-                ViewBag.Estado = "Initialization Success";
-                //EnableButtons(true);
-
-                // FDU03, FDU04 or higher
-                //if (device_name >= SGFPMDeviceName.DEV_FDU03)
-                //    CheckBoxAutoOn.Enabled = true;
+                m_ImageWidth = pInfo.ImageWidth;
+                m_ImageHeight = pInfo.ImageHeight;
             }
-            else
-                DisplayError("OpenDevice()", iError);
-            return iError;
         }
-
-
-        void DisplayError(string funcName, int iError)
+        private void GenerarHuella()
         {
-            string text = "";
+            int iError;
+            int elap_time;
+            Byte[] fp_image;
+            //Cursor.Current = Cursors.WaitCursor;
 
-            switch (iError)
+            elap_time = Environment.TickCount;
+            fp_image = new Byte[m_ImageWidth * m_ImageHeight];
+
+            iError = m_FPM.GetImage(fp_image);
+
+
+            if (iError == (int)SGFPMError.ERROR_NONE)
             {
-                case 0:                             //SGFDX_ERROR_NONE				= 0,
-                    text = "Error none";
-                    break;
-
-                case 1:                             //SGFDX_ERROR_CREATION_FAILED	= 1,
-                    text = "Can not create object";
-                    break;
-
-                case 2:                             //   SGFDX_ERROR_FUNCTION_FAILED	= 2,
-                    text = "Function Failed";
-                    break;
-
-                case 3:                             //   SGFDX_ERROR_INVALID_PARAM	= 3,
-                    text = "Invalid Parameter";
-                    break;
-
-                case 4:                          //   SGFDX_ERROR_NOT_USED			= 4,
-                    text = "Not used function";
-                    break;
-
-                case 5:                                //SGFDX_ERROR_DLLLOAD_FAILED	= 5,
-                    text = "Can not create object";
-                    break;
-
-                case 6:                                //SGFDX_ERROR_DLLLOAD_FAILED_DRV	= 6,
-                    text = "Can not load device driver";
-                    break;
-                case 7:                                //SGFDX_ERROR_DLLLOAD_FAILED_ALGO = 7,
-                    text = "Can not load sgfpamx.dll";
-                    break;
-
-                case 51:                //SGFDX_ERROR_SYSLOAD_FAILED	   = 51,	// system file load fail
-                    text = "Can not load driver kernel file";
-                    break;
-
-                case 52:                //SGFDX_ERROR_INITIALIZE_FAILED  = 52,   // chip initialize fail
-                    text = "Failed to initialize the device";
-                    break;
-
-                case 53:                //SGFDX_ERROR_LINE_DROPPED		   = 53,   // image data drop
-                    text = "Data transmission is not good";
-                    break;
-
-                case 54:                //SGFDX_ERROR_TIME_OUT			   = 54,   // getliveimage timeout error
-                    text = "Time out";
-                    break;
-
-                case 55:                //SGFDX_ERROR_DEVICE_NOT_FOUND	= 55,   // device not found
-                    text = "Device not found";
-                    break;
-
-                case 56:                //SGFDX_ERROR_DRVLOAD_FAILED	   = 56,   // dll file load fail
-                    text = "Can not load driver file";
-                    break;
-
-                case 57:                //SGFDX_ERROR_WRONG_IMAGE		   = 57,   // wrong image
-                    text = "Wrong Image";
-                    break;
-
-                case 58:                //SGFDX_ERROR_LACK_OF_BANDWIDTH  = 58,   // USB Bandwith Lack Error
-                    text = "Lack of USB Bandwith";
-                    break;
-
-                case 59:                //SGFDX_ERROR_DEV_ALREADY_OPEN	= 59,   // Device Exclusive access Error
-                    text = "Device is already opened";
-                    break;
-
-                case 60:                //SGFDX_ERROR_GETSN_FAILED		   = 60,   // Fail to get Device Serial Number
-                    text = "Device serial number error";
-                    break;
-
-                case 61:                //SGFDX_ERROR_UNSUPPORTED_DEV		   = 61,   // Unsupported device
-                    text = "Unsupported device";
-                    break;
-
-                // Extract & Verification error
-                case 101:                //SGFDX_ERROR_FEAT_NUMBER		= 101, // utoo small number of minutiae
-                    text = "The number of minutiae is too small";
-                    break;
-
-                case 102:                //SGFDX_ERROR_INVALID_TEMPLATE_TYPE		= 102, // wrong template type
-                    text = "Template is invalid";
-                    break;
-
-                case 103:                //SGFDX_ERROR_INVALID_TEMPLATE1		= 103, // wrong template type
-                    text = "1st template is invalid";
-                    break;
-
-                case 104:                //SGFDX_ERROR_INVALID_TEMPLATE2		= 104, // vwrong template type
-                    text = "2nd template is invalid";
-                    break;
-
-                case 105:                //SGFDX_ERROR_EXTRACT_FAIL		= 105, // extraction fail
-                    text = "Minutiae extraction failed";
-                    break;
-
-                case 106:                //SGFDX_ERROR_MATCH_FAIL		= 106, // matching  fail
-                    text = "Matching failed";
-                    break;
-
+                elap_time = Environment.TickCount - elap_time;
+                DrawImage(fp_image);
+                //StatusBar.Text = "Capture Time : " + elap_time + " ms";
             }
-
-            text = funcName + " Error # " + iError + " :" + text;
             
         }
+
+        private void DrawImage(Byte[] imgData)//, PictureBox picBox)
+        {
+            int colorval;
+            Bitmap bmp = new Bitmap(m_ImageWidth, m_ImageHeight);
+            //picBox.Image = (Image)bmp;
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    colorval = (int)imgData[(j * m_ImageWidth) + i];
+                    bmp.SetPixel(i, j, Color.FromArgb(colorval, colorval, colorval));
+                }
+            }
+            //picBox.Refresh();
+        }
+
+
 
     }
 }
